60410ba59495cce73664004022443226547b4d05
nite-crawler <25994096+nite-crawler@users.noreply.github.com>
2025-08-12 23:25:01 -0500

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -16,7 +16,7 @@ const About = () => {
             ABOUT ME
           </h2>
           <p className="text-xl text-muted-foreground max-w-3xl mx-auto">
-            Hi Hennabees! I'm Swathi, a passionate henna artist rooted in tradition, dedicated to sharing the ancient art of mehndi/hennai with the world through creativity, skill, and heartfelt respect.
+            Hi Hennabees! I'm Swathi, a passionate henna artist rooted in tradition, dedicated to sharing the ancient art of mehndi/henna with the world through creativity, skill, and heartfelt respect.
           </p>
         </div>
 
