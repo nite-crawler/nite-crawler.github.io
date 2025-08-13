b2938d633fb66e9659f3361380646b698b6b2df6
nite-crawler <25994096+nite-crawler@users.noreply.github.com>
2025-08-13 07:55:01 -0500

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -23,10 +23,10 @@ const About = () => {
                   <div className="px-0 py-0 rounded-sm">
                     <p className="font-medium text-[#272725] py-px">
 OWNER &amp; FOUNDER</p>
-                    <div className="flex items-start gap-2 w-1 h-2"></div>
+                    <div className="flex items-start gap-1 w-1 h-1"></div>
                     <p style={{
                     color: '#D17046'
-                  }} className="font-medium text-[3] flex items-start gap-3 px-0 py-0 my-0 text-[#cb6829]">Lead Artist</p>
+                  }} className="font-medium text-[3] px-0 py-0 my-0 text-[#cb6829]">Lead Artist</p>
                   </div>
                 </div>
               </div>
