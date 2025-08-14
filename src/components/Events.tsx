98f441b08061ef90b4d4e80d5ec3f33a9d161e92
nite-crawler <25994096+nite-crawler@users.noreply.github.com>
2025-08-14 10:33:24 -0500

    Update Events.tsx

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -31,7 +31,7 @@ const Events = () => {
       price: "$65",
       category: "Art Class"
     },
-    {
+  /*  {
      id: 3,
      title: "Kids Henna Fun Day",
      date: new Date(2025, 7, 29), // August 29, 2025
@@ -41,7 +41,7 @@ const Events = () => {
      description: "Child-friendly henna designs and cultural storytelling.",
      price: "$35",
      category: "Kids Event"
-    }
+    } */
   ];
 
   const selectedDateEvents = events.filter(event => 
