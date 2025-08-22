516bfb4f2f9f75889eb7bc0000fef2f2d2c0b2c7
ssn <56628614+ssntosan@users.noreply.github.com>
2025-08-21 23:39:50 -0500

    Update Events.tsx

--- src/components/Events.tsx
+++ src/components/Events.tsx
@@ -25,25 +25,25 @@ const Events = () => {
   const calendarRef = useRef<HTMLDivElement>(null);
 
   const events = [
-    // {
-    //   id: 1,
-    //   title: "Henna Kala @Haymarket",
-    //   date: new Date(2025, 7, 16), // August 16, 2025
-    //   time: "10:00 AM - 12:30 PM",
-    //   location: "Ten Thousand Villages, 140 N 8th St #125, Lincoln, NE 68508, United States.",
-    //   capacity: "Walk-in",
-    //   description: "Fresh and beautiful henna designs while you shop at Haymarket Farmer's Market!",
-    //   category: "Pop-up Henna Booth"
-    // },
-    // {
-    //   id: 2,
-    //   title: "2025 Harvest Moon Festival",
-    //   date: new Date(2025, 9, 5), // October 5, 2025
-    //   time: "4:00 PM - 7:00 PM",
-    //   location: "Antelope Park Bandshell, 1630 Memorial Dr, Lincoln, NE 68502",
-    //   capacity: "Walk-in",
-    //   description: "Create beautiful mandala designs inspired by traditional henna patterns.",
-    //   category: "Public Event "
+    {
+      id: 1,
+      title: "Henna Kala @Haymarket",
+      date: new Date(2025, 7, 16), // August 16, 2025
+      time: "10:00 AM - 12:30 PM",
+      location: "Ten Thousand Villages, 140 N 8th St #125, Lincoln, NE 68508, United States.",
+      capacity: "Walk-in",
+      description: "Fresh and beautiful henna designs while you shop at Haymarket Farmer's Market!",
+      category: "Pop-up Henna Booth"
+    },
+    {
+      id: 2,
+      title: "2025 Harvest Moon Festival",
+      date: new Date(2025, 9, 5), // October 5, 2025
+      time: "4:00 PM - 7:00 PM",
+      location: "Antelope Park Bandshell, 1630 Memorial Dr, Lincoln, NE 68502",
+      capacity: "Walk-in",
+      description: "Create beautiful mandala designs inspired by traditional henna patterns.",
+      category: "Public Event "
     // },
     // {
     //  id: 3,
@@ -55,7 +55,7 @@ const Events = () => {
     //  description: "Child-friendly henna designs and cultural storytelling.",
     //  price: "$35",
     //  category: "Kids Event"
-    // } 
+    } 
   ];
 
   const selectedDateEvents = events.filter(event => 
